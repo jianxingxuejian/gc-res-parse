376367cd284a45c896a8b41e66d4d04838272e40
null <975959057@qq.com>
2022-10-18 12:55:17 +0800

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    es2022: true
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ],
-  rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off'
-  }
+    root: true,
+    env: {
+        node: true,
+        es2022: true
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended'
+    ],
+    rules: {
+        'prettier/prettier': 'error',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off'
+    }
 }
