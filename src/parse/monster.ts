51f1ddad85a29df9bca1618a33d2881700a0a6a8
null <975959057@qq.com>
2022-11-09 13:56:32 +0800

    feat: add monster name translation

--- src/parse/monster.ts
+++ src/parse/monster.ts
@@ -1,27 +1,58 @@
 import { groupBy } from 'lodash-es'
-import { readJson, parse } from '@/utils'
+import { readJson, parse, textMap } from '@/utils'
 
 interface MonsterExcelConfigData extends JsonObject {
     id: number
     monsterName: string
     type: string
-    ai: string
+    nameTextMapHash: number
+    describeId?: number
+    descNameHash?: number
+    specialNameHash?: number
+}
+
+interface MonsterDescribeExcelConfigData extends JsonObject {
+    id: number
+    nameTextMapHash: number
+    titleID: number
+    specialNameLabID: number
 }
 
 export function parseMonster() {
     const monsterData = readJson<MonsterExcelConfigData[]>('ExcelBinOutput/MonsterExcelConfigData.json')
     if (!monsterData) return
 
-    const monsterItem: Record<string, Record<string, string>> = {}
-    Object.entries(
-        groupBy(
-            monsterData.sort((a, b) => a.id - b.id),
-            'type'
-        )
-    ).forEach(([k, v]) => {
-        monsterItem[k] = {}
-        v.forEach(({ id, monsterName }) => (monsterItem[k][id] = monsterName))
+    const monsterDescData = readJson<MonsterDescribeExcelConfigData[]>(
+        'ExcelBinOutput/MonsterDescribeExcelConfigData.json'
+    )
+    if (!monsterDescData) return
+
+    monsterData.forEach(({ describeId }, index) => {
+        if (!describeId) return
+
+        const desc = monsterDescData.find(item => item.id === describeId)
+        if (!desc) return
+
+        monsterData[index].descNameHash = desc.nameTextMapHash
     })
 
-    parse('monsterItem.json', monsterItem)
+    const group = groupBy(
+        monsterData.sort((a, b) => a.id - b.id),
+        'type'
+    )
+
+    Object.keys(textMap).forEach(locale => {
+        const monsterItem: Record<string, Record<string, string>> = {}
+        Object.entries(group).forEach(([k, v]) => {
+            monsterItem[k] = {}
+            v.forEach(({ id, nameTextMapHash, monsterName, descNameHash }) => {
+                let name = textMap[locale][nameTextMapHash] || (descNameHash && textMap[locale][descNameHash])
+                if (name) {
+                    name += ` (${monsterName})`
+                }
+                monsterItem[k][id] = name || monsterName
+            })
+        })
+        parse(locale + '/monsterItem.json', monsterItem)
+    })
 }
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,6 @@
 type Folder = 'res' | 'old' | 'new' | 'diff' | 'merge'
 
-type Json = string | number | boolean | null | Json[] | JsonObject
+type Json = string | number | boolean | null | undefined | Json[] | JsonObject
 
 type JsonObject = {
     [x in string]: Json
