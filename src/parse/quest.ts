1942d48908bd7973eed9f0ae659c6ba9148ffca1
null <975959057@qq.com>
2022-12-04 15:52:08 +0800

    feat: update quest parse

--- src/parse/quest.ts
+++ src/parse/quest.ts
@@ -1,87 +1,80 @@
-import { readDir, readJson, replaceToBlank, includesArray, textMap, parse } from '@/utils'
+import { groupBy } from 'lodash-es'
+import { readJson, replaceToBlank, includesArray, textMap, parse } from '@/utils'
 
-interface QuestData {
-    id: number
-    type: string
+interface QuestExcelConfigData extends JsonObject {
+    mainId: number | null
+    subId: number
+    order: number
     showType: string
-    subQuests?: { subId: number; order: number; showType: string; descTextMapHash?: number }[]
-    titleTextMapHash?: number
     descTextMapHash?: number
 }
 
-type Quest = {
+type QuestInfo = {
     id: number
-    type: string
+    titleHash: number
     hidden?: true
     test?: true
     unreleased?: true
-    titleHash?: number
-    descHash?: number
-    children?: {
-        subId: number
-        order: number
-        hidden?: true
-        test?: true
-        unreleased?: true
-        descHash?: number
-    }[]
+    children?: { subId: number; order: number; descHash?: number; hidden?: true; test?: true; unreleased?: true }[]
+}
+
+interface QuestData {
+    id: number
+    titleTextMapHash: number
+    showType: string
 }
 
 export function parseQuest() {
-    const fileName = 'BinOutput/Quest/'
-    const nameList = readDir(fileName)
-    const questArray: Quest[] = []
-    const questItem_CHS: Record<string, string> = {}
+    const questData = readJson<QuestExcelConfigData[]>('ExcelBinOutput/QuestExcelConfigData.json')
+    if (!questData) return
 
+    const questInfos: QuestInfo[] = []
     const textMapCHS = textMap['zh-CN']
+    const questItem_CHS: Record<string, string> = {}
+    const group = groupBy(questData, 'mainId')
+    Object.entries(group).forEach(([k, v]) => {
+        if (k !== 'null') {
+            const name = `BinOutput/Quest/${k}.json`
+            const read = readJson<QuestData>(name)
+            if (!read) return true
 
-    nameList.forEach(item => {
-        const read = readJson<QuestData>(fileName + item)
-        if (!read) return true
-
-        const { id, type, showType, subQuests, titleTextMapHash, descTextMapHash } = read
-
-        const text = (titleTextMapHash && textMapCHS[titleTextMapHash]) || ''
-
-        let desc = descTextMapHash && textMapCHS[descTextMapHash]
-        if (text === desc) {
-            desc = undefined
-        } else {
-            desc = replaceToBlank(desc, hiddenStrs.concat(testStrs).concat('QUEST_HIDDEN'))
-            descTextMapHash && desc && (questItem_CHS[descTextMapHash] = desc)
-        }
-
-        const { hidden, test, unreleased, newText } = parseText(text, showType)
-
-        titleTextMapHash && newText && (questItem_CHS[titleTextMapHash] = newText)
-
-        const quest: Quest = {
-            id,
-            type,
-            hidden,
-            test,
-            unreleased,
-            titleHash: titleTextMapHash,
-            descHash: descTextMapHash,
-            children:
-                subQuests &&
-                subQuests
+            const { id, titleTextMapHash, showType } = read
+            const text = (titleTextMapHash && textMapCHS[titleTextMapHash]) || ''
+            const { hidden, test, unreleased, newText } = parseText(text, showType)
+            newText && (questItem_CHS[titleTextMapHash] = newText)
+            const questInfo = {
+                id,
+                hidden,
+                test,
+                unreleased,
+                titleHash: titleTextMapHash,
+                children: v
                     .sort((a, b) => a.order - b.order)
-                    .map(({ subId, descTextMapHash, order, showType }) => {
+                    .map(({ subId, order, descTextMapHash, showType }) => {
                         const text = (descTextMapHash && textMapCHS[descTextMapHash]) || ''
                         const { hidden, test, unreleased, newText } = parseText(text, showType)
                         if (descTextMapHash && newText) {
                             questItem_CHS[descTextMapHash] = newText
+                        } else {
+                            descTextMapHash = undefined
                         }
                         return { subId, order, hidden, test, unreleased, descHash: descTextMapHash }
                     })
+            }
+            questInfos.push(questInfo)
+        } else {
+            v.forEach(({ subId, descTextMapHash, showType }) => {
+                const text = (descTextMapHash && textMapCHS[descTextMapHash]) || ''
+                const { hidden, test, unreleased, newText } = parseText(text, showType)
+                if (descTextMapHash && newText) {
+                    questInfos.push({ id: subId, hidden, test, unreleased, titleHash: descTextMapHash })
+                    questItem_CHS[descTextMapHash] = newText
+                }
+            })
         }
-        questArray.push(quest)
     })
 
-    questArray.sort((a, b) => a.id - b.id)
-    parse('quest.json', questArray)
-
+    parse('questInfo.json', questInfos)
     parse('zh-CN/questItem.json', questItem_CHS)
 
     Object.keys(textMap).forEach(locale => {
