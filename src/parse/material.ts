0108e98850a7a74b3d02acba7bc86dbe07b7a93f
null <975959057@qq.com>
2023-01-02 19:28:17 +0800

    update

--- src/parse/material.ts
+++ src/parse/material.ts
@@ -1,5 +1,5 @@
 import { groupBy } from 'lodash-es'
-import { readJson, textMap, parse } from '@/utils'
+import { readJson, textMap, parse, replaceToBlank } from '@/utils'
 
 interface MaterialExcelConfigData {
     id: number
@@ -16,9 +16,10 @@ export function parseMaterial() {
     Object.keys(textMap).forEach(locale => {
         const materialItem: Record<string, Record<string, string>> = {}
         Object.entries(group).forEach(([k, v]) => {
-            materialItem[k] = {}
+            const groupName = replaceToBlank(k, 'MATERIAL_')
+            materialItem[groupName] = {}
             v.forEach(({ id, nameTextMapHash }) => {
-                materialItem[k][id] = textMap[locale][nameTextMapHash]
+                materialItem[groupName][id] = textMap[locale][nameTextMapHash]
             })
         })
         parse(locale + '/materialItem.json', materialItem)
